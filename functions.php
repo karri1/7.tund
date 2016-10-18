42d1f6f0f9dddda66032db2c4f688473e3b46f78
Karin Rikkinen <karri1@users.noreply.github.com>
2016-10-18 14:23:18 +0300

    interests

--- functions.php
+++ functions.php
@@ -143,3 +143,121 @@ function saveCar ($plate, $color) {
 		return $input;
 	}
 	
+	function getAllInterests() {
+		
+		$database = "if16_romil";
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $database);
+		
+		$stmt = $mysqli->prepare("
+			SELECT id, interest
+			FROM interests
+		");
+		echo $mysqli->error;
+		
+		$stmt->bind_result($id, $interest);
+		$stmt->execute();
+		
+		
+		//tekitan massiivi
+		$result = array();
+		
+		// tee seda seni, kuni on rida andmeid
+		// mis vastab select lausele
+		while ($stmt->fetch()) {
+			
+			//tekitan objekti
+			$i = new StdClass();
+			
+			$i->id = $id;
+			$i->interest = $interest;
+		
+			array_push($result, $i);
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+		
+		return $result;
+	}
+	
+	function getAllUserInterests() {
+		
+		$database = "if16_karin";
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $database);
+		
+		$stmt = $mysqli->prepare("
+			SELECT interest FROM interests
+			JOIN user_interests 
+			ON interests.id=user_interests.interest_id
+			WHERE user_interests.user_id = ?
+		");
+		echo $mysqli->error;
+		$stmt->bind_param("i", $_SESSION["userId"]);
+		
+		$stmt->bind_result($interest);
+		$stmt->execute();
+		
+		
+		//tekitan massiivi
+		$result = array();
+		
+		// tee seda seni, kuni on rida andmeid
+		// mis vastab select lausele
+		while ($stmt->fetch()) {
+			
+			//tekitan objekti
+			$i = new StdClass();
+			
+			$i->interest = $interest;
+		
+			array_push($result, $i);
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+		
+		return $result;
+	}
+	
+	function saveUserInterest ($interest) {
+		
+		$database = "if16_karin";
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $database);
+
+		$stmt = $mysqli->prepare("
+			SELECT id FROM user_interests 
+			WHERE user_id=? AND interest_id=?
+		");
+		$stmt->bind_param("ii", $_SESSION["userId"], $interest);
+		$stmt->bind_result($id);
+		
+		$stmt->execute();
+		
+		if ($stmt->fetch()) {
+			// oli olemas juba selline rida
+			echo "juba olemas";
+			// pärast returni midagi edasi ei tehta funktsioonis
+			return;
+			
+		} 
+		
+		$stmt->close();
+		
+		// kui ei olnud siis sisestan
+		
+		$stmt = $mysqli->prepare("
+			INSERT INTO user_interests
+			(user_id, interest_id) VALUES (?, ?)
+		");
+		
+		echo $mysqli->error;
+		
+		$stmt->bind_param("ii", $_SESSION["userId"], $interest);
+		
+		if ($stmt->execute()) {
+			echo "salvestamine õnnestus";
+		} else {
+			echo "ERROR ".$stmt->error;
+		}
+		
+	}
