75665053d486d3d4823122cb24f83ca79f41feff
Karin Rikkinen <karri1@users.noreply.github.com>
2016-10-18 13:57:43 +0300

    added previous lesson

---
+++ functions.php
@@ -0,0 +1,145 @@
+<?php
+
+require("../../config.php");
+session_start();
+
+function signUp ($email, $password) {
+	
+	$database = "if16_karin";
+	$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $database);
+		
+	$stmt = $mysqli->prepare("INSERT INTO user_sample (email, password) VALUES (?, ?)");
+	echo $mysqli -> error;   //see on igaks juhuks
+		
+	$stmt -> bind_param("ss", $email, $password ); 
+
+		if ($stmt->execute()) {
+			echo "salvestamine õnnestus";
+		} else {
+			echo "ERROR ".$stmt->error;
+		}
+		
+		
+	$stmt->close();
+	$mysqli->close();
+	
+}
+
+function login($email, $password){
+	
+	$error = "";
+	
+	$database = "if16_karin";
+	$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $database);
+		$stmt = $mysqli->prepare("
+		SELECT id, email, password, created 
+		FROM user_sample
+		WHERE email = ?");
+	
+		echo $mysqli->error;
+		
+		//asendan küsimärgi
+		$stmt->bind_param("s", $email);
+		
+		//määran väärtused muutujatesse
+		$stmt->bind_result($id, $emailFromDb, $passwordFromDb, $created);
+		$stmt->execute();
+		
+		//andmed tulid andmebaasist või mitte
+		// on tõene kui on vähemalt üks vaste
+		if($stmt->fetch()){
+			
+			//oli sellise meiliga kasutaja
+			//password millega kasutaja tahab sisse logida
+			$hash = hash("sha512", $password);
+			if ($hash == $passwordFromDb) {
+				echo "Kasutaja logis sisse ".$id;
+				
+				//määran sessiooni muutujad, millele saan ligi teistelt lehtedelt
+				$_SESSION["userId"] = $id;
+				$_SESSION["userEmail"] = $emailFromDb;
+				$_SESSION["message"] = "<h1>Tere tulemast!</h1>";
+				header("Location: data.php");
+			}else {
+				$error = "vale parool";
+			}
+			
+			
+		} else {
+			
+			// ei leidnud kasutajat selle meiliga
+			$error = "Ei ole sellist emaili";
+		}
+		
+		return $error;
+	}
+
+function saveCar ($plate, $color) {
+		
+		$database = "if16_karin";
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $database);
+
+		$stmt = $mysqli->prepare("INSERT INTO cars_and_colors (plate, color) VALUES (?, ?)");
+	
+		echo $mysqli->error;
+		
+		$stmt->bind_param("ss", $plate, $color);
+		
+		if($stmt->execute()) {
+			echo "salvestamine õnnestus";
+		} else {
+		 	echo "ERROR ".$stmt->error;
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+		
+	}
+	
+	function getAllCars() {
+		
+		$database = "if16_karin";
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $database);
+		
+		$stmt = $mysqli->prepare("
+			SELECT id, plate, color
+			FROM cars_and_colors
+		");
+		echo $mysqli->error;
+		
+		$stmt->bind_result($id, $plate, $color);
+		$stmt->execute();
+		
+		
+		//tekitan massiivi
+		$result = array();
+		
+		// tee seda seni, kuni on rida andmeid
+		// mis vastab select lausele
+		while ($stmt->fetch()) {
+			
+			//tekitan objekti
+			$car = new StdClass();
+			
+			$car->id = $id;
+			$car->plate = $plate;
+			$car->carColor = $color;
+			
+			//echo $plate."<br>";
+			// iga kord massiivi lisan juurde nr märgi
+			array_push($result, $car);
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+		
+		return $result;
+	}
+	
+	function cleanInput($input){
+		$input = trim($input);
+		$input = stripslashes($input);
+		$input = htmlspecialchars($input);
+		return $input;
+	}
+	
